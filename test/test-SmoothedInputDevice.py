bbe4ea8ecf176a462e930f0e34133906061f17e2
Nicolas Rabier <nicolas.rabier@gmail.com>
2019-03-30 18:52:03 +1100

    Configured sensor gpio

--- test/test-SmoothedInputDevice.py
+++ test/test-SmoothedInputDevice.py
@@ -1,6 +1,7 @@
 from gpiozero import SmoothedInputDevice
+from time import sleep
 
-sensor = SmoothedInputDevice(17,)
+sensor = SmoothedInputDevice(17,pull_up=True,queue_len=1,sample_wait=0.0)
 
 
 print("=== start test ===")
@@ -8,8 +9,21 @@ print("=== start test ===")
 sensor._queue.start()
 print("Sensor Active: ", sensor.is_active)
 
+print("Sensor value:", sensor.value)
+
 sensor.wait_for_inactive(1)
 print("after wait for inactive")
 
+
+print("Sensor value: ", sensor.value)
+
 sensor.wait_for_active(1)
 print("after wait for active")
+
+previousvalue = 0
+while True:
+	currentvalue = sensor.value
+	if currentvalue != previousvalue:
+		print("Value:", currentvalue, "Sound detected!" if currentvalue == 1 else "------------------")
+		sleep(2)
+	previousvalue=currentvalue
