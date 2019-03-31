2490827ca99d1cfa1021bc72f87bf97c946882cc
nicolas.rabier <nicolas.rabier@gmail.com>
2019-03-31 13:21:46 +1100

    start queuing first

--- test/testSmoothedInputDevice.py
+++ test/testSmoothedInputDevice.py
@@ -4,10 +4,9 @@ from time import sleep
 sensor = SmoothedInputDevice(17,pull_up=True,queue_len=1,sample_wait=0.0)
 
 print("=== start test ===")
-print("Sensor Active: ", sensor.is_active)
-print("Sensor value:", sensor.value)
 print("Queue Starts")
 sensor._queue.start()
+
 print("Sensor Active: ", sensor.is_active)
 print("Sensor value:", sensor.value)
 
