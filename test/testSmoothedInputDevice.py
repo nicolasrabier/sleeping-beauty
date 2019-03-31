6141f5ada2c7acb58341744192a15cf075d477df
nicolas.rabier <nicolas.rabier@gmail.com>
2019-03-31 13:01:30 +1100

    tweaking smoothed input device settings

--- test/testSmoothedInputDevice.py
+++ test/testSmoothedInputDevice.py
@@ -3,27 +3,22 @@ from time import sleep
 
 sensor = SmoothedInputDevice(17,pull_up=True,queue_len=1,sample_wait=0.0)
 
-
 print("=== start test ===")
-
+print("Sensor Active: ", sensor.is_active)
+print("Sensor value:", sensor.value)
+print("Queue Starts")
 sensor._queue.start()
 print("Sensor Active: ", sensor.is_active)
-
 print("Sensor value:", sensor.value)
-
-sensor.wait_for_inactive(1)
-print("after wait for inactive")
-
-
-print("Sensor value: ", sensor.value)
-
-sensor.wait_for_active(1)
-print("after wait for active")
 
 previousvalue = 0
 while True:
 	currentvalue = sensor.value
 	if currentvalue != previousvalue:
-		print("Value:", currentvalue, "Sound detected!" if currentvalue == 1 else "------------------")
-		sleep(2)
+		# print("Sound detected!" if currentvalue == 1 else "------------------")
+		if currentvalue == 1:
+			print("Sound detected!")
+			sleep(3)
+		else:
+			print("------------------")
 	previousvalue=currentvalue
