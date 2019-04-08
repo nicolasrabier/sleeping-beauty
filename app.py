9db3b5107abd7c43a86f0e3a1bdc46f5c1e462b3
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-08 18:34:52 +1000

    test first webpage & webservice

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello world'
+	return render_template("index.html")
+
+@app.route('/v1/status')
+def status():
+	return "{'status':'OK'}";
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
