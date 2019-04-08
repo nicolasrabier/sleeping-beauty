9a0eceba999a919da7672c01faec0b7e51044c49
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-08 23:25:30 +1000

    add app.routes

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("dashboard.html")
+
+@app.route('/webservices')
+def webservices():
+	return render_template("webservices.html")
+
+@app.route('/help')
+def webservices():
+	return render_template("help.html")
 
 @app.route('/v1/status')
 def status():
-	return "{'status':'OK'}";
+	return '{"status":"OK"}';
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
