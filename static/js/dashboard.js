1cb6c7cf98e10284d888d6711780ef212b3622da
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-13 23:23:38 +1000

    add confirmation box before powering off the pi

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -8,8 +8,28 @@ $('#turnvolumeup').click(function(){
     document.location.href = '';
 });
 $('#poweroff').click(function(){
-    document.location.href = '/v1/poweroff';
+    ConfirmDialog('Confirmation','Are you sure to power off the device?',function(){document.location.href = '/v1/poweroff';});
 });
 $('#status').click(function(){
     document.location.href = '/v1/status';
 });
+
+function ConfirmDialog(title, message, yesCallback){
+    $('<div></div>').appendTo('body')
+                    .html('<div><h6>'+message+'</h6></div>')
+                    .dialog({
+                        modal: true, title: title, zIndex: 10000, autoOpen: true,
+                        width: 'auto', resizable: false,
+                        buttons: {
+                            Yes: yesCallback(),
+                            No: function () {                                                                 
+                                $(this).dialog("close");
+                            }
+                        },
+                        close: function (event, ui) {
+                            $(this).remove();
+                        }
+                    });
+    }
+    
+    
