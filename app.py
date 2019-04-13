726a4931d4e12efe0caa1dc66f72da8dba302a5f
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-13 20:13:15 +1000

    implement shutting down feature

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+import subprocess
+from sys import stdout
 
 app = Flask(__name__)
 
@@ -14,9 +16,15 @@ def webservices():
 def help():
 	return render_template("help.html")
 
+@app.route('/v1/switchoff', methods=['GET'])
+def switchoff():
+	cmdCommand = "shutdown -h now"
+	process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+	return jsonify(status="SHUTTING_DOWN", process_pid=process.pid)
+
 @app.route('/v1/status', methods=['GET'])
 def status():
-	return jsonify(status='OK')
+	return jsonify(status='RUNNING')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
