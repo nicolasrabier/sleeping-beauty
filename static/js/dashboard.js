1f561041a525d6b31ed727f434ee1c47b63d1bc9
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-14 00:03:32 +1000

    return alert instead of powering off pi

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,35 +1,41 @@
-$('#startstopmonitoring').click(function(){
+$('#startstopmonitoring').click(function() {
     document.location.href = '';
 });
-$('#turnvolumedown').click(function(){
+$('#turnvolumedown').click(function() {
     document.location.href = '';
 });
-$('#turnvolumeup').click(function(){
+$('#turnvolumeup').click(function() {
     document.location.href = '';
 });
-$('#poweroff').click(function(){
-    ConfirmDialog('Confirmation','Are you sure to power off the device?',function(){document.location.href = '/v1/poweroff';});
+$('#poweroff').click(function() {
+    ConfirmDialog('Confirmation', 'Are you sure to power off the device?',
+        function() {
+            //document.location.href = '/v1/poweroff';
+            alert('HERE');
+        });
 });
-$('#status').click(function(){
+$('#status').click(function() {
     document.location.href = '/v1/status';
 });
 
-function ConfirmDialog(title, message, yesCallback){
+function ConfirmDialog(title, message, yesCallback) {
     $('<div></div>').appendTo('body')
-                    .html('<div><h6>'+message+'</h6></div>')
-                    .dialog({
-                        modal: true, title: title, zIndex: 10000, autoOpen: true,
-                        width: 'auto', resizable: false,
-                        buttons: {
-                            Yes: yesCallback(),
-                            No: function () {                                                                 
-                                $(this).dialog("close");
-                            }
-                        },
-                        close: function (event, ui) {
-                            $(this).remove();
-                        }
-                    });
-    }
-    
-    
+        .html('<div><h6>' + message + '</h6></div>')
+        .dialog({
+            modal: true,
+            title: title,
+            zIndex: 10000,
+            autoOpen: true,
+            width: 'auto',
+            resizable: false,
+            buttons: {
+                Yes: yesCallback(),
+                No: function() {
+                    $(this).dialog("close");
+                }
+            },
+            close: function(event, ui) {
+                $(this).remove();
+            }
+        });
+}
