5dc1d88e501e5ea428fe5388a73f80a4d6a64243
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-13 18:45:09 +1000

    fix status json return OK

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def help():
 
 @app.route('/v1/status', methods=['GET'])
 def status():
-	return jsonify(status=OK)
+	return jsonify(status='OK')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
