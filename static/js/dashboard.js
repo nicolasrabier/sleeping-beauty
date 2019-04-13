d2f445cca60dc0db1f35efb4a8962cc7cb7fe598
nicolas.rabier <nicolas.rabier@gmail.com>
2019-04-13 22:19:39 +1000

    add jquery and boostrap

---
+++ static/js/dashboard.js
@@ -0,0 +1,15 @@
+$('#startstopmonitoring').click(function(){
+    window.location = '';
+});
+$('#turnvolumedown').click(function(){
+    window.location = '';
+});
+$('#turnvolumeup').click(function(){
+    window.location = '';
+});
+$('#poweroff').click(function(){
+    window.location = '/v1/poweroff';
+});
+$('#status').click(function(){
+    window.location = '/v1/status';
+});
